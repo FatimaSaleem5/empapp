c7aa7bf1f2f0ad246213c084ba963c8f8a69c837
FatimaSaleem5 <fatima@gallopade.com>
2024-08-20 00:39:18 +0500

    connecting java to mysql

--- src/com/sjprogramming/empapp/DBConnections.java
+++ src/com/sjprogramming/empapp/DBConnections.java
@@ -9,11 +9,11 @@ public class DBConnections {
     {
         try {
             //load driver
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             //get connection
             String url = "jdbc:mysql://localhost:3306/employeeDB?useSSL=false&serverTimezone=UTC";
             String username = "root";
-            String password = "root_pass_1234";
+            String password = "admin123";
             con= DriverManager.getConnection(url,username,password);
         }catch (Exception ex)
         {
